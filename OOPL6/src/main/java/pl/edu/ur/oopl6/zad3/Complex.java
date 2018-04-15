432e855a6f7f1d76d533d3a54c0ab7c294d80fb1
JGrzybowski08 <jakub.grzybowski8@gmail.com>
2018-04-15 22:12:51 +0200

    .

---
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad3/Complex.java
@@ -0,0 +1,16 @@
+package pl.edu.ur.oopl6.zad3;
+
+public class Complex {
+
+    public static double modul(double x, double y){
+        return Math.sqrt((Math.pow(x,2)+Math.pow(y,2)));
+    }
+
+   public static double sprzezenie(double y){
+        return -y;
+   }
+
+   /*public static double zamianaAnaW(){
+
+    }*/
+}
---
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad3/Menu.java
@@ -0,0 +1,10 @@
+package pl.edu.ur.oopl6.zad3;
+
+public class Menu {
+    public static void main(String[] args){
+
+        System.out.println("Modul: "+Complex.modul(2,5));
+
+        System.out.println("Sprzezenie: 2+("+Complex.sprzezenie(-2)+"i)");
+    }
+}
