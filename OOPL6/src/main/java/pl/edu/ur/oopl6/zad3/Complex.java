4b7e67d3b23be5a7b6ada6b9a44122e7edada5bc
Jakub Grzybowski <37274710+JGrzybowski08@users.noreply.github.com>
2018-04-15 23:42:41 +0200

    Update Complex.java

--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad3/Complex.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad3/Complex.java
@@ -1,16 +1,36 @@
 package pl.edu.ur.oopl6.zad3;
 
-public class Menu {
-    public static void main(String[] args){
+public class Complex {
 
-        System.out.println("Modul: "+Complex.modul(2,5));
+    public static double modul(double x, double y){
+        return Math.sqrt((Math.pow(x,2)+Math.pow(y,2)));
+    }
 
-        System.out.println("Sprzezenie: 2+("+Complex.sprzezenie(-2)+"i)");
+   public static double sprzezenie(double y){
+        return -y;
+   }
 
-        Complex.zamianaAnaW(2,5);
+   public static void zamianaAnaW(double x, double y){
+        double mod=modul(x,y);
+        double cos = x/mod;
+        double sin = y/mod;
+        double tg = sin/cos;
+        double kat = Math.atan(tg);
+        System.out.println("Algebraiczna: "+x+"+("+y+"i)");
+        System.out.println("Wykladnicza: "+mod+"*e^"+kat+"i");
+        }
 
-        Complex.zamianaWnaA(2, 0.5);
+   public static void zamianaWnaA(double mod, double katrad){
+        double kat=Math.toDegrees(katrad);
+        double x=mod*Math.cosh(kat);
+        double y=mod*Math.sinh(kat);
+        System.out.println("Wykladnicza: "+mod+"*e^"+kat+"i");
+        System.out.println("Algebraiczna: "+x+"+("+y+"i)");
+   }
 
-        Complex.dodawanie(2, 3, 4, 5);
+    public static void dodawanie(double x, double y, double a, double b){
+        double wa=x+a;
+        double wb=y+b;
+        System.out.println("Suma: "+wa+"+("+wb+"i)");
     }
 }
