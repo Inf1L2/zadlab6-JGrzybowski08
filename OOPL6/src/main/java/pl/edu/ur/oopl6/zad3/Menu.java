1795ff9c1418f47fcbdbd56ab579d7e1f120446b
Jakub Grzybowski <37274710+JGrzybowski08@users.noreply.github.com>
2018-04-15 23:42:10 +0200

    Update Menu.java

--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad3/Menu.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad3/Menu.java
@@ -6,5 +6,11 @@ public class Menu {
         System.out.println("Modul: "+Complex.modul(2,5));
 
         System.out.println("Sprzezenie: 2+("+Complex.sprzezenie(-2)+"i)");
+
+        Complex.zamianaAnaW(2,5);
+
+        Complex.zamianaWnaA(2, 0.5);
+
+        Complex.dodawanie(2, 3, 4, 5);
     }
 }
